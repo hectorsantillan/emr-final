80497baa6a28308c0a8d3b67aab38e2d1a021626
hectorsantillan <32185262+hectorsantillan@users.noreply.github.com>
2017-10-14 21:41:31 +0800

    patients

--- my-app/sw-precache-config.js
+++ my-app/sw-precache-config.js
@@ -14,6 +14,7 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/src/*.html',
+    '/src/*.json',
     'bower_components/webcomponentsjs/webcomponents-loader.js',
     'images/*',
     'manifest.json',
